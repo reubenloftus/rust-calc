2ad90f9b08e931d4cbcc546d3d252e417a435894
Reuben Loftus <reubenloftus@icloud.com>
2025-07-15 19:02:44 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,558 @@
+const Data = {
+  "wooden-door": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 1,
+      time: "2s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "20 Explosives, 5 Cloth, 1 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 1,
+      time: "4s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "10 Explosives, 2 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 18,
+      time: "15s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "180 Gunpowder, 90 Metal Frags, 180 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 5,
+      time: "20s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "700 Gunpowder, 10 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 2,
+      time: "30s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "2 Small Stash, 8 Rope, 8 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 10,
+      time: "40s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "300 Gunpowder, 250 Metal Frags, 10 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 9,
+      time: "45s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "360 Gunpowder, 180 Metal Frags, 9 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 8,
+      time: "45s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "480 Gunpowder, 160 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 87,
+      time: "120s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "435 Stone, 435 Gunpowder"
+    }
+  ],
+
+  "sheet-metal-door": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 2,
+      time: "4s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "40 Explosives, 10 Cloth, 2 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 2,
+      time: "8s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "20 Explosives, 4 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 36,
+      time: "30s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "360 Gunpowder, 180 Metal Frags, 360 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 10,
+      time: "40s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "1400 Gunpowder, 20 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 4,
+      time: "60s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "4 Small Stash, 16 Rope, 16 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 20,
+      time: "80s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "600 Gunpowder, 500 Metal Frags, 20 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 18,
+      time: "90s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "720 Gunpowder, 360 Metal Frags, 18 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 16,
+      time: "90s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "960 Gunpowder, 320 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 174,
+      time: "240s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "870 Stone, 870 Gunpowder"
+    }
+  ],
+
+  "armored-door": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 3,
+      time: "6s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "60 Explosives, 15 Cloth, 3 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 3,
+      time: "12s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "30 Explosives, 6 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 54,
+      time: "45s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "540 Gunpowder, 270 Metal Frags, 540 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 15,
+      time: "60s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "2100 Gunpowder, 30 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 6,
+      time: "90s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "6 Small Stash, 24 Rope, 24 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 30,
+      time: "120s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "900 Gunpowder, 750 Metal Frags, 30 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 27,
+      time: "135s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "1080 Gunpowder, 540 Metal Frags, 27 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 24,
+      time: "135s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "1440 Gunpowder, 480 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 261,
+      time: "360s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "1305 Stone, 1305 Gunpowder"
+    }
+  ],
+
+  "wooden-wall": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 1,
+      time: "2s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "20 Explosives, 5 Cloth, 1 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 1,
+      time: "4s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "10 Explosives, 2 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 18,
+      time: "15s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "180 Gunpowder, 90 Metal Frags, 180 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 5,
+      time: "20s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "700 Gunpowder, 10 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 2,
+      time: "30s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "2 Small Stash, 8 Rope, 8 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 10,
+      time: "40s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "300 Gunpowder, 250 Metal Frags, 10 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 9,
+      time: "45s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "360 Gunpowder, 180 Metal Frags, 9 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 8,
+      time: "45s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "480 Gunpowder, 160 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 87,
+      time: "120s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "435 Stone, 435 Gunpowder"
+    }
+  ],
+
+  "sheet-metal-wall": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 2,
+      time: "4s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "40 Explosives, 10 Cloth, 2 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 2,
+      time: "8s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "20 Explosives, 4 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 36,
+      time: "30s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "360 Gunpowder, 180 Metal Frags, 360 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 10,
+      time: "40s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "1400 Gunpowder, 20 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 4,
+      time: "60s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "4 Small Stash, 16 Rope, 16 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 20,
+      time: "80s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "600 Gunpowder, 500 Metal Frags, 20 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 18,
+      time: "90s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "720 Gunpowder, 360 Metal Frags, 18 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 16,
+      time: "90s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "960 Gunpowder, 320 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 174,
+      time: "240s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "870 Stone, 870 Gunpowder"
+    }
+  ],
+
+  "armored-wall": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 3,
+      time: "6s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "60 Explosives, 15 Cloth, 3 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 3,
+      time: "12s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "30 Explosives, 6 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 54,
+      time: "45s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "540 Gunpowder, 270 Metal Frags, 540 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 15,
+      time: "60s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "2100 Gunpowder, 30 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 6,
+      time: "90s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "6 Small Stash, 24 Rope, 24 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 30,
+      time: "120s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "900 Gunpowder, 750 Metal Frags, 30 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 27,
+      time: "135s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "1080 Gunpowder, 540 Metal Frags, 27 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 24,
+      time: "135s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "1440 Gunpowder, 480 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 261,
+      time: "360s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "1305 Stone, 1305 Gunpowder"
+    }
+  ],
+
+  "high-external-wall": [
+    {
+      weapon: "C4",
+      img: "images/C4.png",
+      qty: 4,
+      time: "8s",
+      materials: "20 Explosives, 5 Cloth, 1 Tech Trash",
+      total: "80 Explosives, 20 Cloth, 4 Tech Trash"
+    },
+    {
+      weapon: "Rocket",
+      img: "images/Rocket.png",
+      qty: 4,
+      time: "16s",
+      materials: "10 Explosives, 2 Metal Pipes",
+      total: "40 Explosives, 8 Metal Pipes"
+    },
+    {
+      weapon: "Explosive Ammo",
+      img: "images/ExplosiveAmmo.png",
+      qty: 72,
+      time: "60s",
+      materials: "10 Gunpowder, 5 Metal Frags, 10 Sulfur",
+      total: "720 Gunpowder, 360 Metal Frags, 720 Sulfur"
+    },
+    {
+      weapon: "HV Rocket",
+      img: "images/HV_Rocket.png",
+      qty: 20,
+      time: "80s",
+      materials: "140 Gunpowder, 2 Metal Pipes",
+      total: "2800 Gunpowder, 40 Metal Pipes"
+    },
+    {
+      weapon: "Satchel Charge",
+      img: "images/Satchel.png",
+      qty: 8,
+      time: "120s",
+      materials: "1 Small Stash, 4 Rope, 4 Beancan",
+      total: "8 Small Stash, 32 Rope, 32 Beancan"
+    },
+    {
+      weapon: "F1 Grenade",
+      img: "images/F1Grenade.png",
+      qty: 40,
+      time: "160s",
+      materials: "30 Gunpowder, 25 Metal Frags, 1 Explosive",
+      total: "1200 Gunpowder, 1000 Metal Frags, 40 Explosives"
+    },
+    {
+      weapon: "40MM HE Grenade",
+      img: "images/Grenade.png",
+      qty: 36,
+      time: "180s",
+      materials: "40 Gunpowder, 20 Metal Frags, 1 Explosive",
+      total: "1440 Gunpowder, 720 Metal Frags, 36 Explosives"
+    },
+    {
+      weapon: "Beancan Grenade",
+      img: "images/Beancan.png",
+      qty: 32,
+      time: "180s",
+      materials: "60 Gunpowder, 20 Metal Frags",
+      total: "1920 Gunpowder, 640 Metal Frags"
+    },
+    {
+      weapon: "Handmade Shell",
+      img: "images/HandmadeShell.png",
+      qty: 348,
+      time: "480s",
+      materials: "5 Stone, 5 Gunpowder",
+      total: "1740 Stone, 1740 Gunpowder"
+    }
+  ]
+};
+
+
+const tableBody = document.getElementById("table-body");
+const select = document.getElementById("target-select");
+
+function renderTable(target) {
+  const items = data[target];
+  tableBody.innerHTML = "";
+
+  items.forEach(item => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>
+        <div class="weapon-cell">
+          <img src="${item.img}" alt="${item.weapon}" class="weapon-img" />
+          <span>${item.weapon}</span>
+        </div>
+      </td>
+      <td>${item.qty}</td>
+      <td>${item.time}</td>
+      <td>${item.materials}</td>
+      <td>${item.total}</td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+renderTable(select.value);
+
+select.addEventListener("change", (e) => {
+  renderTable(e.target.value);
+});
